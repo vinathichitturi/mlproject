5cbbea8e12e69cc29384b5941c62e434fc831609
Vinathi <chitturivinathichoudary@gmail.com>
2025-10-10 13:11:27 +0530

    recent changes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -10,17 +10,18 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-        
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
+    
     return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='vinathi',
-    author_email='vinathichoudary@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
